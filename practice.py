6a30403e6745617cdcae39b85044df64ddeb8907
jyoti <jshindems@gmail.com>
2020-10-31 20:37:37 -0400

    added dictionary code

--- practice.py
+++ practice.py
@@ -58,3 +58,55 @@ print('The result is {r: 1.2f}'.format(r=result1))
 print(f'Hello, my name is {name}')
 
 print(f'Hello, my name is {name}, lives in {city}')
+
+my_list= [1,2,3,4,5,6]
+
+print(len(my_list))
+print(my_list)
+
+my_list[0] = 0
+print(my_list)
+
+my_list.append(7)
+print(my_list)
+
+#remove last element of list
+my_list.pop()
+print(my_list)
+
+new_list = [ 'e', 'g', 'k', 'f', 'l', 's']
+new_list.sort()
+print(new_list)
+
+num_list = [9,1, 6, 5, 2, 1]
+num_list.sort()
+print(num_list)
+
+num_list.reverse()
+print(num_list)
+
+prices = {'Strawberry': 10, "Mango": 5, "Carrot": 25, "Banana": 3}
+
+print(prices['Mango'])
+
+d = {'k1': 123, 'k2': [0,1,2], 'k3': { 'insideKey': 100}, 'k3': [ 'a', 'b', 'c']}
+
+print(d['k2'])
+print(d['k2'][2])
+print(d['k3'][2].upper())
+
+#adding value to dictionary
+
+d['k4'] = 400
+
+print(d)
+
+#print dict keys
+print(d.keys())
+
+#print dict values
+print(d.values())
+
+#print keys and values
+print(d.items())
+
